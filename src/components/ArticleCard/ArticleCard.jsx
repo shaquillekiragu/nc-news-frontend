f954d1c87c81d269a3ec8dcc94df260ec869e605
shaquillekiragu <skiragu77@gmail.com>
2024-10-05 16:53:38 +0100

    Applied flex styling to article cards

--- src/components/ArticleCard/ArticleCard.jsx
+++ src/components/ArticleCard/ArticleCard.jsx
@@ -11,13 +11,15 @@ function ArticleCard({ article, index }) {
     viewportWidth = window.innerWidth;
   });
 
+  const formattedCreatedAt = new Date(article.created_at).toLocaleString();
+
   if (index === 0) {
     return (
       <>
         <Link to={path}>
           <section className="topArticleCard">
             <div className="layerOne">
-              <p className="item author">By: {article.author}</p>
+              <p className="item author">Author: {article.author}</p>
               <p className="item topic" id="topicTag">
                 {article.topic}
               </p>
@@ -26,20 +28,20 @@ function ArticleCard({ article, index }) {
               <h3 className="item title">{article.title}</h3>
             </div>
             <div className="layerThree">
-              <p className="item body">
-                {article.body.length <= 500
-                  ? article.body
-                  : `${article.body.slice(0, 501)}...`}
-              </p>
               <img
                 className="item thumbnail"
                 src={article.article_img_url}
                 alt="Article thumbnail"
               />
+              <p className="item body">
+                {article.body.length <= 500
+                  ? article.body
+                  : `${article.body.slice(0, 501)}...`}
+              </p>
             </div>
             <div className="layerFour">
-              <p className="item createdAt">Created at: {article.created_at}</p>
-              <p className="item votes">Votes: {article.votes}</p>
+              <p className="item createdAt">{formattedCreatedAt}</p>
+              <p className="item votes">Upvotes: {article.votes}</p>
             </div>
             <div className="layerFive">
               <p className="item commentCount">
@@ -57,7 +59,7 @@ function ArticleCard({ article, index }) {
         <Link to={path}>
           <section className="secondArticleCard">
             <div className="layerOne">
-              <p className="item author">By: {article.author}</p>
+              <p className="item author">Author: {article.author}</p>
               <p className="item topic" id="topicTag">
                 {article.topic}
               </p>
@@ -73,8 +75,8 @@ function ArticleCard({ article, index }) {
               />
             </div>
             <div className="layerFour">
-              <p className="item createdAt">Created at: {article.created_at}</p>
-              <p className="item votes">Votes: {article.votes}</p>
+              <p className="item createdAt">{formattedCreatedAt}</p>
+              <p className="item votes">Upvotes: {article.votes}</p>
             </div>
             <div className="layerFive">
               <p className="item commentCount">
@@ -92,7 +94,7 @@ function ArticleCard({ article, index }) {
         <Link to={path}>
           <section className="articleCard">
             <div className="layerOne">
-              <p className="item author">By: {article.author}</p>
+              <p className="item author">Author: {article.author}</p>
               <p className="item topic" id="topicTag">
                 {article.topic}
               </p>
@@ -108,8 +110,8 @@ function ArticleCard({ article, index }) {
               />
             </div>
             <div className="layerFour">
-              <p className="item createdAt">Created at: {article.created_at}</p>
-              <p className="item votes">Votes: {article.votes}</p>
+              <p className="item createdAt">{formattedCreatedAt}</p>
+              <p className="item votes">Upvotes: {article.votes}</p>
               <p className="item commentCount">
                 Comments: {article.comment_count}
               </p>
